1b10f7112a6e2f0c6bf2fda5a85009f5c05d18a6
Pythonsway <adam@pythonsway.it>
2023-08-20 10:04:22 +0200

    manifest, footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import './Footer.css';
+
 const Footer = () => (
   <div className="footer">
+     <div className="app-name">
+      <p>* The name "Twitt-agra" comes from a monologue in the 2010 movie <a rel="noreferrer noopener" href="https://www.imdb.com/title/tt1231587" target="_blank">Hot Tub Time Machine</a>:</p>
+      <blockquote>
+        <p>-What about Twitter? Whatever the fuck that is.</p>
+        <p>-Hey, we could combine Twitter with fucking Viagra.</p>
+        <p>-Twitt-agra.</p>
+        <p>...</p>
+        <cite>Lou</cite>      
+      </blockquote>
+    </div>
+    <hr />
     <div className="">
-      <p> by <a href="https://pythonsway.it" target="_blank" rel="noopener noreferrer">Python&#39;s way</a></p>
+      <p>by <a href="https://pythonsway.it" target="_blank" rel="noopener noreferrer">Python&#39;s way</a></p>
     </div>
   </div>
 );
